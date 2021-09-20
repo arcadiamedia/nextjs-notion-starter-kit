470c99925a12017cd73f4935d29a4fb8258345f4
arcadiamedia <loong@outlook.sg>
2021-09-21 10:13:13 +1200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'a95e43dfa47f4cc79babdb06c3f127ba',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'SANHE CONSTRUCTION',
+  domain: 'https://www.notion.so/sanhe/SANHE-CONSTRUCTION-a95e43dfa47f4cc79babdb06c3f127ba',
+  author: 'Rachel',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'SANHE CONSTRUCTION',
+  socialImageTitle: 'Sanhe Construction',
+  socialImageSubtitle: 'Sanhe Construction',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
